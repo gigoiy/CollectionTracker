d936f3c0992e097efcea03331e165c315cf544b8
AlexOMara00 <alexander.omara@snhu.edu>
2024-04-11 05:06:46 -0400

    Some changes to array, started setting up code transfer between forms. Also trying to set up array to label info based on button press

--- Form2.cs
+++ Form2.cs
@@ -21,13 +21,6 @@ namespace ProjectAlexKadyn
             string collectionName { get; set; }
             string collectionValue { get; set; }
             
-            string[,] item; //starts 2d array, 1st will be the item name, 2nd will be the price, description, and model#
-            public CollectionName(string Name)
-            {
-                collectionName = Name;
-            }
-            // made the constructor so that when the new collection is called, it will automatically be set
-        }
         public frmViewCollection()
         {
             InitializeComponent();
